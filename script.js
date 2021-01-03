4391ef172fa9c278e599792afb1a3065fb9e32b8
James <67014970+JLoweDev@users.noreply.github.com>
2021-01-03 14:51:53 +0000

    Function to show time on screen complete

--- script.js
+++ script.js
@@ -4,3 +4,23 @@ const greeting = document.getElementById('greeting');
 const name = document.getElementById('name');
 const focus = document.getElementById('focus');
 
+// Show Time
+function showTime() {
+    let today = new Date();
+    let hour = today.getHours();
+    let min = today.getMinutes();
+
+    // Update Time in HTML
+    time.innerHTML = `${hour}<span>:</span>${min}`;
+
+    // Repeats showTime every 1000 milliseconds
+    setTimeout(showTime, 1000);
+}
+
+// Add Zeros
+function addZero() {
+
+}
+
+// Init
+showTime();
