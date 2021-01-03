6bdf276b07a9b68df29f3013011ad8f621a13250
James <67014970+JLoweDev@users.noreply.github.com>
2021-01-03 16:12:47 +0000

    Background and Greeting function created

--- script.js
+++ script.js
@@ -22,5 +22,29 @@ function addZero(n) {
     return (parseInt(n, 10) < 10 ? '0' : '') + n;
 }
 
+// Set Background and Greeting
+function setBgGreet() {
+    let today = new Date();
+    let hour = today.getHours();
+    if(hour >= 6 && hour < 11) {
+        // Morning
+        document.body.style.backgroundImage = 'url("img/morning.jpg")';
+        greeting.textContent = 'Good Morning';
+    } else if(hour >= 11 && hour < 18) {
+        // Midday
+        document.body.style.backgroundImage = 'url("img/midday.jpg")';
+        greeting.textContent = 'Good Day';
+    } else if(hour >= 18 && hour < 22) {
+        // Evening
+        document.body.style.backgroundImage = 'url("img/evening.jpg")';
+        greeting.textContent = 'Good Evening';
+    } else {
+        // Night
+        document.body.style.backgroundImage = 'url("img/night.jpg")';
+        greeting.textContent = 'Good Night';
+    }
+}
+
 // Init
 showTime();
+setBgGreet();
