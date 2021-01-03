39eb32bcc56ad848c86555ddaf4dc6268472b6f1
James <67014970+JLoweDev@users.noreply.github.com>
2021-01-03 14:43:17 +0000

    DOM elements added to JS

---
+++ script.js
@@ -0,0 +1,6 @@
+// DOM Elements
+const time = document.getElementById('time');
+const greeting = document.getElementById('greeting');
+const name = document.getElementById('name');
+const focus = document.getElementById('focus');
+
