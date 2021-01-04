a2f4322f3e210b828a516a0e2e662f086c5acef4
James <67014970+JLoweDev@users.noreply.github.com>
2021-01-04 15:14:31 +0000

    Removed irrelevent text shadow from JS

--- script.js
+++ script.js
@@ -39,13 +39,11 @@ function setBgGreet() {
         document.body.style.backgroundImage = 'url("img/evening.jpg")';
         greeting.textContent = 'Good Evening';
         document.body.style.color = 'white';
-        document.body.style.textShadow = 'black 0px 0px 7px';
     } else {
         // Night
         document.body.style.backgroundImage = 'url("img/night.jpg")';
         greeting.textContent = 'Good Night';
         document.body.style.color = 'white';
-        document.body.style.textShadow = 'black 0px 0px 7px';
     }
     setTimeout(setBgGreet, 1000);
 }
